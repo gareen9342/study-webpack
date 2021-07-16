e9af59e2f08a9add15d50b69bb8bd11c6a5e7dc9
조가린 <41724184+gareen9342@users.noreply.github.com>
2021-07-16 23:43:34 +0900

    .

--- lint/.eslintrc.js
+++ lint/.eslintrc.js
@@ -1,10 +1,28 @@
 module.export= {
-    extends : [
-        //https://eslint.org/docs/rules/
-        "eslint:recommended" // 해당 옵션을 붙여서 추천하는 옵션을 적용시킨다. 
-    ]
+    // extends : [
+    //     //https://eslint.org/docs/rules/
+    //     "eslint:recommended" // 해당 옵션을 붙여서 추천하는 옵션을 적용시킨다. 
+    // ]
     // rules : {
     //     "no-unexpected-multiline" : "error",
     //     "no-extra-semi" : "error"
     // }
+    env: {
+        browser: true,
+        es6: true,
+        node: true
+      },
+      extends: "eslint:recommended",
+      globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
+      },
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module"
+      },
+      plugins: ["prettier"], // prettier옵션 추가.
+      rules: {
+        "prettier/prettier": "error"
+      }
 }
